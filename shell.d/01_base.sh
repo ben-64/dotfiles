acb5918d9ac96db1389d0c32a0db0e1af0472520
ben64 <ben64@time0ut.org>
2024-06-29 12:06:14 +0200

    Base function script load first

---
+++ shell.d/01_base.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if_exist() {
+    which $1 > /dev/null 2>/dev/null
+    if [ $? -eq 0 ]; then
+        eval $2
+    else
+        [ $# -eq 3 ] && eval $3
+    fi
+}
+
+exist() {
+    which $1 > /dev/null 2>/dev/null
+    return $?
+}
+
