3553b561659e06af822ede031f36f97dbc9f6b42
ben64 <ben64@time0ut.org>
2024-07-06 14:22:20 +0200

    Remove missing-field warning for lua

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -118,6 +118,9 @@ return {
               completion = {
                 callSnippet = "Replace",
               },
+              diagnostics = {
+                disable = { "missing-fields" },
+              },
             },
           },
         })
