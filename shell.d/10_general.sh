47456eced5d4757a0002ebd9d1a33bf0abf58d5b
benny <benoit.camredon@airbus.com>
2026-01-27 14:51:01 +0100

    Do not load python env by default + set EZA env variable

--- shell.d/10_general.sh
+++ shell.d/10_general.sh
@@ -1,13 +1,9 @@
 #!/bin/zsh
 
-# If there is a venv, do load it
-if [ -f $PYENV/bin/activate ]; then
-  source $PYENV/bin/activate
-fi  
-
 # Some ENV
 export BAT_THEME=tokyonight_night
 export BAT_THEME=gruvbox-dark
+export EZA_CONFIG_DIR=~/.config/eza/
 
 # thefuck alias
 if exist thefuck; then
